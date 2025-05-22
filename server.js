ad51508ae13eb7e0f25a66d9cc88931e09d8cc93
devwaghela123 <143732122+devwaghela123@users.noreply.github.com>
2025-05-22 22:01:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static('public'));
+app.use('/uploads', express.static('uploads'));
+
 
 // Multer setup
 const storage = multer.diskStorage({
